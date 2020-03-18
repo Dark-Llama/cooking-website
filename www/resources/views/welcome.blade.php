6d416533754641b93ebc8370f06080d1cc5febf2
Dark-Llama <40619577+Dark-Llama@users.noreply.github.com>
2020-03-19 09:45:50 +1300

    Started Building Homepage
    Added fonts, changed navbar items.

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -4,17 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Recipiely</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Nunito','Open Sans', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -48,7 +50,7 @@
                 font-size: 84px;
             }
 
-            .links > a {
+            .navbar > a {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
@@ -81,18 +83,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Recipiely
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="navbar">
+                    <a href="https://laravel.com/docs">Recipies</a>
+                    <a href="https://laracasts.com">Drinks</a>
+                    <a href="https://laravel-news.com">Home</a>
+                    <a href="https://blog.laravel.com">Videos</a>
+                    <a href="https://nova.laravel.com">Healthy Eating</a>
                 </div>
             </div>
         </div>
