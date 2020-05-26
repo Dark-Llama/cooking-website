8dc936a5cfadd98fcdd13cc6eaa9d7f4eefb68ad
Dark-Llama <40619577+Dark-Llama@users.noreply.github.com>
2020-05-26 19:04:47 +1200

    Restructure for deployment

---
+++ resources/views/healthy.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="parallax" style="background: url('assets/img/bruschetta-italianfood-tomato-cooking.jpg');">
+		<div class="container">
+
+			<h1>Healthy Eating</h1>
+
+			<div class="row">
+
+				<div class="col-md-6">
+					<h3 class="pt-3">If you want to eat more healthy, use our Healthy Sorting system and this handyg guide to do so easily.</h3>
+					<p>p>
+				</div>
+
+				<div class="col-md-6">
+					<p>If you want to get started just sort the recipes by healthy only, this will only show you recipes that have been deemed healthy according this this guide.</p>
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4">
+					<h4>Breakfasts are hard</h4>
+					<p>So we make them easy, our expansive database of user contributed breakfast recipes will give you the best start to the day.</p>
+				</div>
+
+				<div class="col-md-4">
+					<h4>Lunch with the whanau? We got you covered</h4>
+					<p>If you need to whip up something quick for yourself, or are preparing a extensive meal for your whanau we have recipes to suit everyones needs, by sorting healthy recipes only you can make sure that everyone is getting what they need.</p>
+				</div>
+
+				<div class="col-md-4">
+					<h4>Whether it's dinner with friends or just for one, look no further!</h4>
+					<p>With recipes ranging in difficulty and size, you can find something thats delicious and healthy for anyone!</p>
+				</div>
+
+			</div>
+		</div>
+
+
+	</div>
+@endsection
